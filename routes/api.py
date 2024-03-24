9a507a714e3c8fc22b91a00a17e6538d79d2af5a
wendellemosmoura <wendel_lemos_cg@hotmail.com>
2024-03-24 01:18:37 -0300

    Adicionado os códigos de status no response (200, 400) Correção para os avisos FutureWarning e SettingWithCopyWarning Ignorando o aviso XMLParsedAsHTMLWarning Atualização das libs do projeto Utilizando a classe correta (ValueError) no tratamento das exceções

--- routes/api.py
+++ routes/api.py
@@ -1,9 +1,16 @@
-from flask import Blueprint
+import json
+from io import StringIO
+
 import pandas as pd
 import requests
-import json
+from bs4 import XMLParsedAsHTMLWarning
+from flask import Blueprint, make_response
+import warnings
 
 api_bp = Blueprint('api', __name__)
+
+# Ignorando o XMLParsedAsHTMLWarning, pois é feita a análise de um HTML e não de um XML
+warnings.simplefilter(action='ignore', category=XMLParsedAsHTMLWarning)
 
 
 @api_bp.route('/')
@@ -15,45 +22,59 @@ def api_home():
 def feriados(ano):
     ano = str.strip(ano)
     if ano.isdigit() is False or ano is None or int(ano) < 1:
-        return 'Insira um mês válido'
+        return make_response('Insira um ano válido', 400)
     try:
         # Response
         page = requests.get(f"https://www.anbima.com.br/feriados/fer_nacionais/{str.strip(ano)}.asp")
+
+        # Criando um objeto que simula um arquivo
+        page_buffer = StringIO(page.text)
+
         # Dataframe gerado a partir do html da página.
-        df_feriados = pd.read_html(page.text, match='Data', index_col=None, header=0)
-        df_feriados = df_feriados[2]
+        df_feriados_ano = pd.read_html(page_buffer, match='Data', index_col=None, header=0)
+        df_feriados_ano = df_feriados_ano[2]
+
         # Usando pandas.to_datetime() para converter de string para datetime64
-        df_feriados["Data"] = pd.to_datetime(df_feriados["Data"], format="%d/%m/%y")
+        df_feriados_ano["Data"] = pd.to_datetime(df_feriados_ano["Data"], format="%d/%m/%y")
+
         # Formatando a data no formato DD/MM/YYYY
-        df_feriados["Data"] = df_feriados["Data"].dt.strftime('%d/%m/%Y')
-        result = df_feriados.to_json(orient="index")
+        df_feriados_ano["Data"] = df_feriados_ano["Data"].dt.strftime('%d/%m/%Y')
+        result = df_feriados_ano.to_json(orient="index")
         parsed = json.loads(result)
-        return json.dumps(parsed, indent=4)
-    except Exception as e:
+        return json.dumps(parsed, indent=4), 200
+    except ValueError as e:
         print(e)
-        return 'Sem dados para exibição'
+        return make_response('Sem dados para exibição', 400)
 
 
 @api_bp.route(f'/<ano>/<mes>')
 def feriados_ano_mes(ano, mes):
     mes = str.strip(mes)
     if mes.isdigit() is False or mes is None or int(mes) < 1 or int(mes) > 12:
-        return 'Insira um mês válido'
+        return make_response('Insira um mês válido', 400)
     try:
         # Response
         page = requests.get(f"https://www.anbima.com.br/feriados/fer_nacionais/{ano}.asp")
+
+        # Criando um objeto que simula um arquivo
+        page_buffer = StringIO(page.text)
+
         # Dataframe gerado a partir do html da página.
-        df_feriados = pd.read_html(page.text, match='Data', index_col=None, header=0)
-        df_feriados = df_feriados[2]
+        df_feriados_mes = pd.read_html(page_buffer, match='Data', index_col=None, header=0)
+        df_feriados_mes = df_feriados_mes[2]
+
         # Usando pandas.to_datetime() para converter de string para datetime64
-        df_feriados["Data"] = pd.to_datetime(df_feriados["Data"], format="%d/%m/%y")
-        # Filtrando por mês
-        df = df_feriados[df_feriados['Data'].dt.month == int(mes)]
+        df_feriados_mes["Data"] = pd.to_datetime(df_feriados_mes["Data"], format="%d/%m/%y")
+
+        # Filtrando por mês e usando .copy() para evitar o aviso "SettingWithCopyWarning"
+        df = df_feriados_mes[df_feriados_mes['Data'].dt.month == int(mes)].copy()
+
         # Formatando a data no formato DD/MM/YYYY
         df["Data"] = df["Data"].dt.strftime('%d/%m/%Y')
+
         result = df.to_json(orient="index")
         parsed = json.loads(result)
-        return json.dumps(parsed, indent=4)
-    except Exception as e:
+        return json.dumps(parsed, indent=4), 200
+    except ValueError as e:
         print(e)
-        return 'Sem dados para exibição'
+        return make_response('Sem dados para exibição', 400)
